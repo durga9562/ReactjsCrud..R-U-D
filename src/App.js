bb14ae176db1e9988770afbaa4188439012caa85
Durga Prasad Donga <dp21422@corp.ojas-it.com>
2022-05-07 11:01:18 +0530

    prasad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users,setUser] = useState([])
+  const [name, setName] =useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [userId,setUserId] = useState(null)
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+  function getUsers(){
+    fetch("http://localhost:3000/posts").then((result)=>{
+      result.json().then((resp)=>{
+        setUser(resp)
+        setName(resp[0].name)
+        setMobile(resp[0].mobile)
+        setEmail(resp[0].email)
+        setUserId(resp[0].id)
+      })
+    })
+  }
+
+  function deleteUser(id){
+  fetch(`http://localhost:3000/posts/${id}`,{
+    method:'DELETE'
+  }).then((result)=>{
+    result.json().then((resp)=>{
+      console.warn(resp)
+      getUsers()
+    })
+  })
+  }
+function selectUser(id){
+  let item=users[id-1]
+  setName(item.name)
+  setEmail(item.email)
+  setMobile(item.mobile);
+  setUserId(item.id)
+}
+
+function updateUser(){
+  let item={name,mobile,email}
+  console.warn("item",item)
+  fetch(`http://localhost:3000/posts/${userId}`,{
+    method:'PUT',
+    headers:{
+      'Accept':'application/json',
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify(item)
+  }).then((result)=>{
+    result.json().then((resp)=>{
+      console.warn(resp)
+      getUsers()
+    })
+  })
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Update User Data With API</h1>
+      <table className='table1' border="1 solid" style={{float:'left'}}>
+        <tbody style={{border:"1 solid 2px violet"}}>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Mobile</td>
+            <td>Operations</td>
+          </tr>
+          {
+            users.map((item,i)=>
+            <tr key={i}>
+              <td style={{border:"1 solid 2px violet"}}>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+              <td><button class="btn btn-danger" onClick={()=>deleteUser(item.id)}>DELETE</button></td>
+              <td><button class="btn btn-info" onClick={()=>selectUser(item.id)}>Update</button></td>
+            </tr>
+            )
+          }
+        </tbody>
+      </table>
+      <div>
+        <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/> <br/> <br/>
+        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}/> <br/> <br/>
+        <input type="text" value={mobile} onChange={(e)=>{setMobile(e.target.value)}}/> <br/> <br/>
+        <button  class="btn btn-success" onClick={updateUser}>Update User</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
